9fe93f18e59707ea2a514258b7b5c06d823b5ecf
k464483963 <57167613+k464483963@users.noreply.github.com>
2021-01-28 19:33:39 -0800

    Add files via upload

---
+++ part1/part1-question18.js
@@ -0,0 +1,26 @@
+let statistics = {
+    redCars:21,
+    blueCars:45,
+    greenCars:12,
+    raceCars:5,
+    blackCars:40,
+    rareCars:2
+};
+
+function isOdd(v){
+    if(v%2 == 0){
+        return false;
+    }
+    else
+        return true;
+}
+
+for(const prop in statistics){
+    if(`${prop}`.startsWith('r')){
+        console.log(`statistics.${prop}=${statistics[prop]}`);
+    }
+    else if(isOdd(`${statistics[prop]}`)){
+        console.log(`statistics.${prop}=${statistics[prop]}`);
+    }
+    //console.log(`statistics.${prop}=${statistics[prop]}`);
+}
